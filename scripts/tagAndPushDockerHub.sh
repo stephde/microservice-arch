badba88874b66355a22d040273d3baf4e29bd9b3
Stephan Detje <stephan.detje@student.hpi.uni-potsdam.de>
2019-02-14 13:54:08 +0100

    add docker hub docker compose file

---
+++ scripts/tagAndPushDockerHub.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+REGISTRY_URI=stephde/
+TAG=:v1
+
+docker tag model-maintainer ${REGISTRY_URI}model-maintainer${TAG}
+docker push ${REGISTRY_URI}model-maintainer${TAG}
+
+docker tag kubernetes-consumer ${REGISTRY_URI}kubernetes-consumer${TAG}
+docker push ${REGISTRY_URI}kubernetes-consumer${TAG}
+
+docker tag tracing-example ${REGISTRY_URI}tracing-example${TAG}
+docker push ${REGISTRY_URI}tracing-example${TAG}
+
+docker tag zipkin-consumer ${REGISTRY_URI}zipkin-consumer${TAG}
+docker push ${REGISTRY_URI}zipkin-consumer${TAG}
